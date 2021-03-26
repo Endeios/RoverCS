30b7e086217d041605cd7e8bb01e32584d3a6a25
Endeios <endeios@gmail.com>
2021-03-26 23:54:52 +0100

    Test for LLL, IMPLEMENTATION WITH left rotation map

--- rover-lib/Rover.cs
+++ rover-lib/Rover.cs
@@ -1,35 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace rover_lib
 {
     public class Rover
     {
+        private IDictionary<Direction, Direction> leftRotation = new Dictionary<Direction, Direction>()
+        {
+            { Direction.North , Direction.West },
+            { Direction.West , Direction.South },
+            { Direction.South , Direction.Est },
+            { Direction.Est , Direction.North }
+        };
         Direction currentDirection = Direction.North;
         public Position go(string input)
         {
             if(string.IsNullOrEmpty(input))
                 return new Position(0, 0, Direction.North);
             foreach (char command in input) {
-                if (currentDirection == Direction.Est)
-                {
-                    currentDirection = Direction.North;
-                    continue;
-                }
-                if (currentDirection == Direction.South)
-                {
-                    currentDirection = Direction.Est;
-                    continue;
-                }
-                if (currentDirection == Direction.West)
-                {
-                    currentDirection = Direction.South;
-                    continue;
-                }
-                if (currentDirection == Direction.North)
-                {
-                    currentDirection = Direction.West;
-                    continue;
-                }
+                currentDirection = leftRotation[currentDirection];
             }
             return new Position(0, 0, currentDirection);
         }
